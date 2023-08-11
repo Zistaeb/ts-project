841006cb8bf34f4a9d43c8d0a74a9be8c82f953d
Betty <bperezatencio@gmail.com>
2023-08-11 20:18:37 -0300

    Tipos inferidos "Funcion anonima autoejecutada"

---
+++ distribution/03-typing.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    let myProductName = 'Product 1';
+    let myProductPrice = 123;
+    myProductName = 'change';
+    myProductName.toLowerCase();
+    myProductPrice.toFixed();
+    const myProductStock = 1000;
+})();
---
+++ src/03-typing.ts
@@ -0,0 +1,14 @@
+(() => {                                               //Funcion anonima autoejecutada
+  let myProductName: string = 'Product 1';
+let myProductPrice = 123;
+
+myProductName = 'change';
+myProductName.toLowerCase();
+
+myProductPrice.toFixed();
+
+const myProductStock = 1000;
+})();
+
+
+
