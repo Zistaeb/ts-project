538b4ef69102b739d076895a732a3b8cb5091375
Betty <bperezatencio@gmail.com>
2023-08-11 21:17:59 -0300

    Type: boolean

---
+++ distribution/05-booleanos.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    let isEnable = true;
+    isEnable = false;
+    let isNew = false;
+    console.log('isNew', isNew);
+    isNew = true;
+    console.log('isNew', isNew);
+    const random = Math.random;
+    console.log('random', random);
+})();
---
+++ src/05-booleanos.ts
@@ -0,0 +1,12 @@
+(() => {
+  let isEnable = true;
+  isEnable = false;
+
+  let isNew: boolean = false;
+  console.log('isNew', isNew);
+  isNew = true;
+  console.log('isNew', isNew);
+
+  const random = Math.random;
+  console.log('random', random);
+})();
