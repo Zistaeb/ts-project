bbb1281b128b70640677bf2c224093dd098cb19e
Betty <bperezatencio@gmail.com>
2023-08-12 23:10:20 -0300

    strings

---
+++ distribution/06-strings.js
@@ -0,0 +1,17 @@
+"use strict";
+(() => {
+    let productTitle = 'My amazing product';
+    productTitle = 'My amazing product changed';
+    console.log('productTitle', productTitle);
+    const productDescription = "I'm bla bla bla";
+    console.log('productDescription', productDescription);
+    let productPrice = 100;
+    let isNew = false;
+    const sumary = `
+    title: ${productTitle}
+    description: ${productDescription}
+    price: ${productPrice}
+    isNew: ${isNew}
+  `;
+    console.log(sumary);
+})();
